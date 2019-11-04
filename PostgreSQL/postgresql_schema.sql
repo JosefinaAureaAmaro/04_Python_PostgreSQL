b0b3576461b245dc48993117e0739e8cc3761aa3
JosefinaAureaAmaro <46851546+JosefinaAureaAmaro@users.noreply.github.com>
2019-11-04 16:10:20 -0500

    to update repo with finalized code format

--- PostgreSQL/postgresql_schema.sql
+++ PostgreSQL/postgresql_schema.sql
@@ -4,9 +4,14 @@ CREATE TABLE employee (
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   gender VARCHAR(1) NOT NULL,
-  hire_date DATE NOT NULL,
+  hire_date DATE NOT NULL
 );
 
+CREATE TABLE dept (
+  dept_no VARCHAR(6),
+  dept_name VARCHAR(30),
+  dept_id SERIAL PRIMARY KEY
+);
 
 
 CREATE TABLE dept_emp (
@@ -16,7 +21,7 @@ CREATE TABLE dept_emp (
   from_date DATE NOT NULL,
   to_date DATE,
   dept_id INTEGER,
-  FOREIGN KEY (dept_id) REFERENCES dept(dept_id),
+  FOREIGN KEY (dept_id) REFERENCES dept(dept_id)
 );
 
 
@@ -29,11 +34,7 @@ CREATE TABLE dept_mgr (
   dept_mgr_id SERIAL PRIMARY KEY
 );
 
-CREATE TABLE dept (
-  dept_no VARCHAR(6),
-  dept_name VARCHAR(30),
-  dept_id SERIAL PRIMARY KEY
-);
+
 
 CREATE TABLE titles (
   emp_no INTEGER NOT NULL,
@@ -48,7 +49,7 @@ CREATE TABLE salary (
   FOREIGN KEY (emp_no) REFERENCES employee (emp_no),
   salary INTEGER NOT NULL,
   from_date DATE NOT NULL,
-  to_date DATE,
+  to_date DATE
 );
 
 --common edit made
